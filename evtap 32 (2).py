cb8771c03e0f16b532d01b237a8c96d07bd6e392
HasanAvshar <hesenavs1@gmail.com>
2023-04-29 18:43:15 +0400

    Create evtap 32 (2).py

---
+++ evtap 32 (2).py
@@ -0,0 +1,64 @@
+# class Car:
+#     def __init__(self,name_of_the_model, production_year, manufacturer_company, engine_capacity,color_of_your_car,price):
+#         self.of_the_model = of_the_model
+#         self.production_year = production_year
+#         self.manufacturer_company = manufacturer_company
+#         self.engine_capacity = engine_capacity
+#         self.color_of_your_car = color_of_your_car
+#         self.price = price
+#
+#     def car_information(self):
+#         print(f"Model:\t {self.of_the_model}\n"
+#               f"Product year:\t{self.production_year}\n"
+#               f"Manufacturer company:\t{self.manufacturer_company}\n"
+#               f"Engine capacity:\t{self.engine_capacity}\n"
+#               f"Color of your car:\t{self.color_of_your_car}\n"
+#               f"Price:\t{self.price}\n")
+# user1 = Car("bmw","1922","Bayerische Motoren","320w","red","50k")
+# user2 = Car("prado","1940","Toyota Motor Corporation","350w","white","40k")
+# user3 = Car("tyota prius","1930","StepIt","1000w","yellow","20k")
+#
+# user_list = [user1, user2,user3]
+# for i in user_list:
+#         i.car_information()
+#==================================================================
+# class Book:
+#     def __init__(self,name_of_the_book,graduation_year, publishing_house,genre_of_the_book,the_author,price):
+#         self.name_of_the_book = name_of_the_book
+#         self.graduation_year = graduation_year
+#         self.publishing_house = publishing_house
+#         self.genre_of_the_book = genre_of_the_book
+#         self.the_author = the_author
+#         self.price = price
+#
+#     def Book_information(self):
+#         print(f"Name_book:\t {self.name_of_the_book}\n"
+#               f"Graduation_year:\t{self.graduation_year}\n"
+#               f"Publishing_house:\t{self.publishing_house}\n"
+#               f"Genre of the book:\t{self.genre_of_the_book}\n"
+#               f"The author:\t{self.the_author}\n"
+#               f"Price:\t{self.price}\n")
+# user1 = Book("Harry Potter1","2006","2008","Fantastic","JK.Rowling","12.99")
+# user2 = Book("Harry Potter","2008","2010","Fantastic","JK.Rowling","15")
+# my_list = [user1, user2]
+# for i in user_list:
+#      i.Book_information()
+#============================================================================
+# class Sports_field:
+#     def __init__(self,the_name_of_the_stadium,opening_date,country, city,about_capacity):
+#         self.the_name_of_the_stadium = the_name_of_the_stadium
+#         self.opening_date = opening_date
+#         self.country = country
+#         self.city = city
+#         self.about_capacity = about_capacity
+#
+#     def Sports1_field_information(self):
+#         print(f"Name_book:\t {self.the_name_of_the_stadium}\n"
+#               f"Graduation_year:\t{self.opening_date}\n"
+#               f"Publishing_house:\t{self.country}\n"
+#               f"Genre of the book:\t{self.city}\n"
+#               f"The author:\t{self.about_capacity}\n")
+# user1 = Sports_field("StepAcademy","2005","Baki","Sumqayit","8k")
+# list = [user1]
+# for i in list:
+#     i.Sports1_field_information()
